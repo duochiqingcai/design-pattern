b84c013a626d911c2fb919ae2f2b778ecf3775d7
duochiqingcai <1193686994@qq.com>
2019-02-23 15:44:19 +0800

    commit.

--- sim-mysql-data/src/main/java/com/dcqc/demo/SimMysqlDataApplication.java
+++ sim-mysql-data/src/main/java/com/dcqc/demo/SimMysqlDataApplication.java
@@ -8,6 +8,7 @@ public class SimMysqlDataApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SimMysqlDataApplication.class, args);
+        System.out.println("===============================================启动成功=============================================");
     }
 
 }
