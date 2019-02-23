ce34db1a90c035be204601ac456dfd44b835a9e5
duochiqingcai <1193686994@qq.com>
2019-02-23 23:41:22 +0800

    2/23.

---
+++ sim-mysql-data/src/main/java/com/dcqc/demo/web/OutputTest.java
@@ -0,0 +1,16 @@
+package com.dcqc.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @program: sim-mysql-data
+ * @description
+ * @author: duochiqingcai
+ * @create: 2019-02-23 23:36
+ **/
+@Controller
+@RequestMapping
+public class OutputTest {
+
+}
