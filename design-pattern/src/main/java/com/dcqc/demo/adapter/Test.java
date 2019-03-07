3f8d49e742e083fdc18ceb341d4ab15f5a1c63e0
duochiqingcai <1193686994@qq.com>
2019-03-07 22:50:42 +0800

    添加适配器模式~

---
+++ design-pattern/src/main/java/com/dcqc/demo/adapter/Test.java
@@ -0,0 +1,10 @@
+package com.dcqc.demo.adapter;
+
+/**
+ * @program: design-pattern
+ * @description
+ * @author: duochiqingcai
+ * @create: 2019-03-07 22:42
+ **/
+public class Test {
+}
---
+++ design-pattern/src/main/java/com/dcqc/demo/adapter/TypeC.java
@@ -0,0 +1,10 @@
+package com.dcqc.demo.adapter;
+
+/**
+ * @program: design-pattern
+ * @description
+ * @author: duochiqingcai
+ * @create: 2019-03-07 22:35
+ **/
+public class TypeC {
+}
